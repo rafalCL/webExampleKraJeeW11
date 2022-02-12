c40cea58464aca856797d587aab131784a9d7df0
Rafal <rafal.opial@coderslab.pl>
2022-02-12 10:51:28 +0100

    m3d3t1z1 formularze

---
+++ src/main/java/pl/coderslab/web/post/zad1/Post1Servlet.java
@@ -0,0 +1,18 @@
+package pl.coderslab.web.post.zad1;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/post1")
+public class Post1Servlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String firstName = req.getParameter("firstName");
+        String lastName = req.getParameter("lastName");
+        resp.getWriter().println(String.format("Witaj, %s %s", firstName, lastName));
+    }
+}
